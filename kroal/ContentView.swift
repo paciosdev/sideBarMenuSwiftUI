f1e7108f6cbd92f33db61895e0aec37b63afe5db
Francesco Paciello <kekkopacio@gmail.com>
2021-04-24 00:01:10 +0200

    Initial Commit

---
+++ kroal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kroal
+//
+//  Created by Kekko Paciello on 24/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kroal/kroalApp.swift
@@ -0,0 +1,17 @@
+//
+//  kroalApp.swift
+//  kroal
+//
+//  Created by Kekko Paciello on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct kroalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
